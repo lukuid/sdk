f9e8b46fc846e7826700cad7baee9350e5ebabe3
Johann du Toit <me@johanndutoit.net>
2026-04-29 22:34:04 +0300

    bumped version to 1.0.7

--- src/android/lukuid-sdk/build.gradle.kts
+++ src/android/lukuid-sdk/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "com.lukuid"
-version = (findProperty("VERSION_NAME") as String?) ?: "1.0.6"
+version = (findProperty("VERSION_NAME") as String?) ?: "1.0.7"
 
 android {
     namespace = "com.lukuid.sdk"
