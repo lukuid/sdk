b9b7b341553fb27f9e6df6ae72d8f8baefdea928
Johann du Toit <me@johanndutoit.net>
2026-04-30 05:22:05 +0300

    release: v1.0.13

--- src/android/lukuid-sdk/build.gradle.kts
+++ src/android/lukuid-sdk/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "com.lukuid"
-version = (findProperty("VERSION_NAME") as String?) ?: "1.0.12"
+version = (findProperty("VERSION_NAME") as String?) ?: "1.0.13"
 
 android {
     namespace = "com.lukuid.sdk"
